a17a4eed0b0f4b569179f77985c6951e30ae1ad2
endalamawy <endalamawteka23@gmail.com>
2025-10-19 12:02:31 -0500

    Update components and styles for deployment

--- src/Components/Rows/Row/Row.jsx
+++ src/Components/Rows/Row/Row.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import "./Row.css";
-import axios from '../../../utils/axios'
+import "./row.css";
+import axios from '../../../Utils/axios'
 import movieTrailer from "movie-trailer";
 import YouTube from "react-youtube";
 
