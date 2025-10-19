e9fc8b22eb7f85612e078fd32f99022e4aa81f20
endalamawy <endalamawteka23@gmail.com>
2025-10-18 23:59:01 -0500

    Replace old project with myMovie-clone files

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+base :"myproject2-March-2025"
+
+})
