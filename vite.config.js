a1176bc6871417d2ef5f3cd469afca69107ef0b2
endalamawy <endalamawteka23@gmail.com>
2025-10-24 23:24:05 -0500

    Prepare for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-
-base :"myproject2-March-2025"
-
-})
-
-
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
+// // https://vite.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   base: "/myproject2-March-2025/", // ✅ MUST match your repo name
+
+// base :"myproject2-March-2025"
+
 // })
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/myproject2-March-2025/", // ✅ MUST match your repo name
+})
 
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
