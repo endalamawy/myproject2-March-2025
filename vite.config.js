6720d2e5485b4f4aa2953c56a372cbe744944232
endalamawy <endalamawteka23@gmail.com>
2025-10-24 23:11:49 -0500

    Configure Vite base and deploy settings

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 
-// base :"myproject2-March-2025"
+base :"myproject2-March-2025"
 
-// })
+})
 
 
 
