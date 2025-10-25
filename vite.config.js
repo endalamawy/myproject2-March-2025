d0403652b489bbd09d810a41aa7a6e8296490614
endalamawy <endalamawteka23@gmail.com>
2025-10-24 23:07:40 -0500

    Configure Vite base and deploy settings

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,20 @@
 
 // })
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: "/", // ✅ Important: starts and ends with a slash
-})
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/myproject2-March-2025/", // ✅ MUST match your repo name
+// })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/", // ✅ Important: starts and ends with a slash
+// })
